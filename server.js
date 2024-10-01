0d121bdd876f1dec1e2b16a31ffe9659c2176130
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-10-01 05:56:20 +0200

    Create function to read from json file

--- server.js
+++ server.js
@@ -11,6 +11,21 @@ const postsFilePath = path.join(__dirname, 'posts.json');
 app.use(cors());
 app.use(express.json());
 
+// Read posts from the JSON file
+function getPosts() {
+    try {
+      if (fs.existsSync(postsFilePath)) {
+        const data = fs.readFileSync(postsFilePath, 'utf8');
+        return JSON.parse(data);
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.error('Error reading posts file:', error);
+      return [];
+    }
+  }
+  
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
